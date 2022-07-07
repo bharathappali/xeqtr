f1df996dbd09e8ff649ae69124388d3421ec72f5
bharathappali <bharath.appali@gmail.com>
2022-07-08 02:33:45 +0530

    Implemented a singleton pattern for xeqtr core
    Signed-off-by: bharathappali <bharath.appali@gmail.com>

--- xeqtr.go
+++ xeqtr.go
@@ -2,12 +2,36 @@ package xeqtr
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 )
+
+type xeqtr_core struct {
+	capacity int
+}
+
+var initialised_xeqtr_core *xeqtr_core
+var once sync.Once
 
 func Create(capacity int) bool {
 	if capacity <= 0 {
 		return false
 	}
+
+	if capacity > runtime.NumCPU() {
+		fmt.Printf("WARN: capacity %d is greater than CPU's, you might experience a performance issue \n", capacity)
+	}
+
+	once.Do(func() {
+		initialised_xeqtr_core = &xeqtr_core{capacity: capacity}
+	})
 	fmt.Printf("capacity: %d\n", capacity)
 	return true
+}
+
+func GetCapacity() (int, error) {
+	if initialised_xeqtr_core == nil {
+		return 0, fmt.Errorf("unable to get capacity")
+	}
+	return initialised_xeqtr_core.capacity, nil
 }
