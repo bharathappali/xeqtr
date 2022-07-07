b36848545dd1674d9b6c04f4c778b0dabbae4535
bharathappali <bharath.appali@gmail.com>
2022-07-08 03:25:29 +0530

    Formatted errors with fmt errorf
    Signed-off-by: bharathappali <bharath.appali@gmail.com>

--- xeqtr.go
+++ xeqtr.go
@@ -1,7 +1,6 @@
 package xeqtr
 
 import (
-	"errors"
 	"fmt"
 	"runtime"
 	"sync"
@@ -11,7 +10,10 @@ type xeqtr_core struct {
 	capacity int
 }
 
+const max_multiplier int = 4
+
 var default_xeqtr_core_capacity int = runtime.NumCPU()
+var max_xeqtr_core_capacity_limit int = max_multiplier * default_xeqtr_core_capacity
 var initialised_xeqtr_core *xeqtr_core = nil
 var once sync.Once
 
@@ -22,7 +24,12 @@ func Create(capacity int) error {
 	}
 
 	if capacity > default_xeqtr_core_capacity {
-		fmt.Printf("WARN: capacity %d is greater than CPU's, you might experience a performance issue \n", capacity)
+		if capacity <= max_xeqtr_core_capacity_limit {
+			fmt.Printf("WARN: capacity %d is greater than CPU's, you might experience a performance issue \n", capacity)
+		} else {
+			fmt.Errorf("capacity limit exceeded, please set the capacity limit less than or equal to max capacity : %d\n", max_xeqtr_core_capacity_limit)
+		}
+
 	}
 
 	once.Do(func() {
@@ -34,7 +41,7 @@ func Create(capacity int) error {
 
 func GetCapacity() (int, error) {
 	if initialised_xeqtr_core == nil {
-		return -1, errors.New("core unintialised")
+		return -1, fmt.Errorf("core unintialised")
 	}
 	return initialised_xeqtr_core.capacity, nil
 }
